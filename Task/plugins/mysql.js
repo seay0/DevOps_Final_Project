335499f517d8ab7943ee8d70371aac7fc466f638
JEONGDratini <pw050503@gmail.com>
2023-06-23 01:44:29 +0900

    Update Task

--- Task/plugins/mysql.js
+++ Task/plugins/mysql.js
@@ -3,7 +3,7 @@ const fp = require('fastify-plugin')
 //const { HOSTNAME, USERNAME, PASSWORD, DB_NAME } = process.env
 
 module.exports = fp(async function (fastify, opts) {
-    const url = `mysql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOSTNAME}:3306/${process.env.DB_NAME}`
+    const url = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`
     fastify.register(require('@fastify/mysql'), {
         promise: true,
         connectionString: url
